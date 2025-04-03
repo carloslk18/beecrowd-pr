dfc6ae4ab852d7a902cd6175e1707bc8532a923c
Carlos Barbosa <77971569+carloslk18@users.noreply.github.com>
2025-04-02 23:30:00 -0300

    Update Tomadas.cs

--- PrCsharp/Tomadas.cs
+++ PrCsharp/Tomadas.cs
@@ -12,6 +12,25 @@ class Tomadas{
         int rule3 = Int32.Parse(ruleParts[2]);
         int rule4 = Int32.Parse(ruleParts[3]);
 
+        string [] ruleParts = rules.Split(' ');
+        int rule1 = Int32.Parse(ruleParts[0]);
+        int rule2 = Int32.Parse(ruleParts[1]);
+        int rule3 = Int32.Parse(ruleParts[2]);
+        int rule4 = Int32.Parse(ruleParts[3]);string [] ruleParts = rules.Split(' ');
+        int rule1 = Int32.Parse(ruleParts[0]);
+        int rule2 = Int32.Parse(ruleParts[1]);
+        int rule3 = Int32.Parse(ruleParts[2]);
+        int rule4 = Int32.Parse(ruleParts[3]);string [] ruleParts = rules.Split(' ');
+        int rule1 = Int32.Parse(ruleParts[0]);
+        int rule2 = Int32.Parse(ruleParts[1]);
+        int rule3 = Int32.Parse(ruleParts[2]);
+        int rule4 = Int32.Parse(ruleParts[3]);string [] ruleParts = rules.Split(' ');
+        int rule1 = Int32.Parse(ruleParts[0]);
+        int rule2 = Int32.Parse(ruleParts[1]);
+        int rule3 = Int32.Parse(ruleParts[2]);
+        int rule4 = Int32.Parse(ruleParts[3]);
+        
+
         var socket = new List<int>{
             rule1, rule2, rule3, rule4
         };
