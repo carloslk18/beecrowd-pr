cdc23efc7ef9dbe61805dd3744eefd0329ff1fec
Carlos Barbosa <77971569+carloslk18@users.noreply.github.com>
2025-04-09 23:32:33 -0300

    Update Tomadas.cs

--- PrCsharp/Tomadas.cs
+++ PrCsharp/Tomadas.cs
@@ -8,9 +8,7 @@ class Tomadas{
         var rules = Console.ReadLine();
         string [] ruleParts = rules.Split(' ');
         int rule1 = Int32.Parse(ruleParts[0]);
-        int rule2 = Int32.Parse(ruleParts[1]);
-        int rule3 = Int32.Parse(ruleParts[2]);
-        int rule4 = Int32.Parse(ruleParts[3]);    
+        int rule2 = Int32.Parse(ruleParts[1]);   
 
         var socket = new List<int>{
             rule1, rule2, rule3, rule4
