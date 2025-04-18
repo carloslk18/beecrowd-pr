ad7296a24f44a0cff524f73096a00ee5e7a34656
Carlos Barbosa <77971569+carloslk18@users.noreply.github.com>
2025-04-18 14:58:08 -0300

    Update Area.cs

--- PrCsharp/Area.cs
+++ PrCsharp/Area.cs
@@ -32,9 +32,7 @@ public class Area{
         Console.WriteLine($"CIRCULO: {circulo:F3}");
         Console.WriteLine($"TRAPEZIO: {trapezio:F3}");
         Console.WriteLine($"QUADRADO: {quadrado:F3}");
-        Console.WriteLine($"RETANGULO: {retangulo:F3}"); Console.WriteLine($"TRIANGULO: {triangulo:F3}");
-        Console.WriteLine($"CIRCULO: {circulo:F3}");
-        Console.WriteLine($"TRAPEZIO: {trapezio:F3}");
+
 
     }
 
