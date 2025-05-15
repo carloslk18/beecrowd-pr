7e18911bf8291f6044815ffc0ec5a250a3385ea4
Carlos Barbosa <77971569+carloslk18@users.noreply.github.com>
2025-05-14 23:52:38 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿using System;
+using System;
 using BeeCrowd.PrCsharp;
 
 namespace BeeCrowd{
 
 class Program{
     public static void Main(string [] args){
+
+       Bigger calculo = new Bigger();
+       calculo.PR1013v2();
+
+       Bigger calculo = new Bigger();
+       calculo.PR1013v2();
 
        Bigger calculo = new Bigger();
        calculo.PR1013v2();
