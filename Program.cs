c004e2ee454b3f4ea26d6c2b7bd6e114788f7483
Carlos Barbosa <77971569+carloslk18@users.noreply.github.com>
2025-06-05 22:55:58 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,8 +16,6 @@ class Program{
        calculo.PR1013v2();Bigger calculo = new Bigger();
        calculo.PR1013v2();Bigger calculo = new Bigger();
        calculo.PR1013v2();Bigger calculo = new Bigger();
-       calculo.PR1013v2();Bigger calculo = new Bigger();
-       calculo.PR1013v2();
     }
 }
 }
