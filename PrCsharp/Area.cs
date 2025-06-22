6b4ea08b1d666512be3b8ee87db0806372c08f42
Carlos Barbosa <77971569+carloslk18@users.noreply.github.com>
2025-06-22 17:01:45 -0300

    Update Area.cs

--- PrCsharp/Area.cs
+++ PrCsharp/Area.cs
@@ -68,4 +68,15 @@ public class Area{
         double area;
         area = bas * altura;
         return area;public double cRetantulo(double bas, double altura){
+
+              public double cRetantulo(double bas, double altura){
+        double area;
+        area = bas * altura;
+         public double cRetantulo(double bas, double altura){
+        double area;
+        area = bas * altura;
+         public double cRetantulo(double bas, double altura){
+        double area;
+        area = bas * altura;
+       
 }
