47078568c1653287ae1ae1570ac701327c278aab
Carlos Barbosa <77971569+carloslk18@users.noreply.github.com>
2025-08-25 23:52:57 -0300

    Update Distancia.cs

--- PrCsharp/Distancia.cs
+++ PrCsharp/Distancia.cs
@@ -64,7 +64,6 @@ public class Distancia {
 
         Console.WriteLine($"{val} minutos");
             public static void PR1016(){
-        int val = Convert.ToInt32(Console.ReadLine());
     }
 }
 }
