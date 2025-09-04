30b8bc7f18b0b99ed7901335a302cfb925aef427
Carlos Barbosa <77971569+carloslk18@users.noreply.github.com>
2025-09-04 01:47:17 -0300

    Update IdadeDias.cs

--- PrCsharp/IdadeDias.cs
+++ PrCsharp/IdadeDias.cs
@@ -22,6 +22,108 @@ public class IdadeDias{
         Console.WriteLine($"{ano} ano(s)");
         Console.WriteLine($"{mes} mes(es)");
         Console.WriteLine($"{dia} dia(s)");
+
+        public class IdadeDias{
+    
+    public void PR1020(){
+
+        int ano, mes, dia;
+
+        var val = int.Parse(Console.ReadLine());
+
+        ano = val / 365;
+        mes = val % 365 / 30;
+
+        if (val % 365 >= 30){
+            dia = (val % 365) - (30*mes);
+        }
+        else{
+            dia = val % 365;
+        }
+
+        Console.WriteLine($"{ano} ano(s)");
+        Console.WriteLine($"{mes} mes(es)");
+        Console.WriteLine($"{dia} dia(s)");public class IdadeDias{
+    
+    public void PR1020(){
+
+        int ano, mes, dia;
+
+        var val = int.Parse(Console.ReadLine());
+
+        ano = val / 365;
+        mes = val % 365 / 30;
+
+        if (val % 365 >= 30){
+            dia = (val % 365) - (30*mes);
+        }
+        else{
+            dia = val % 365;
+        }
+
+        Console.WriteLine($"{ano} ano(s)");
+        Console.WriteLine($"{mes} mes(es)");
+        Console.WriteLine($"{dia} dia(s)");public class IdadeDias{
+    
+    public void PR1020(){
+
+        int ano, mes, dia;
+
+        var val = int.Parse(Console.ReadLine());
+
+        ano = val / 365;
+        mes = val % 365 / 30;
+
+        if (val % 365 >= 30){
+            dia = (val % 365) - (30*mes);
+        }
+        else{
+            dia = val % 365;
+        }
+
+        Console.WriteLine($"{ano} ano(s)");
+        Console.WriteLine($"{mes} mes(es)");
+        Console.WriteLine($"{dia} dia(s)");public class IdadeDias{
+    
+    public void PR1020(){
+
+        int ano, mes, dia;
+
+        var val = int.Parse(Console.ReadLine());
+
+        ano = val / 365;
+        mes = val % 365 / 30;
+
+        if (val % 365 >= 30){
+            dia = (val % 365) - (30*mes);
+        }
+        else{
+            dia = val % 365;
+        }
+
+        Console.WriteLine($"{ano} ano(s)");
+        Console.WriteLine($"{mes} mes(es)");
+        Console.WriteLine($"{dia} dia(s)");public class IdadeDias{
+    
+    public void PR1020(){
+
+        int ano, mes, dia;
+
+        var val = int.Parse(Console.ReadLine());
+
+        ano = val / 365;
+        mes = val % 365 / 30;
+
+        if (val % 365 >= 30){
+            dia = (val % 365) - (30*mes);
+        }
+        else{
+            dia = val % 365;
+        }
+
+        Console.WriteLine($"{ano} ano(s)");
+        Console.WriteLine($"{mes} mes(es)");
+        Console.WriteLine($"{dia} dia(s)");
     }
 }
 }
